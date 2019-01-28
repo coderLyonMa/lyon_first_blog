efac77a844dbd57554dec9117feb39f88586cb77
coderLyonMa <coderlyonma@gmail.com>
2019-01-28 15:45:55 +0800

    rebuilding done

--- blog/main/views.py
+++ blog/main/views.py
@@ -26,4 +26,4 @@ def index():
         return redirect(url_for('.index'))
     return render_template('index.html',
                            form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False), current_time=datetime.utcnow())
