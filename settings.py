2af156b8af58cc261e97419483ee6af80a08f17a
coderLyonMa <coderlyonma@gmail.com>
2019-02-27 19:48:31 +0800

    deploy to heroku and modified database url config

--- settings.py
+++ settings.py
@@ -38,7 +38,7 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_GREEN_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
     @classmethod
